97b4338ea907794be1143fc953e2e8dcc7b9a65f
cccjjz <2631756191@qq.com>
2024-06-06 00:58:11 +0800

    add user management api and bugfix

--- src/main/java/com/huawei/ibooking/serivce/UserService.java
+++ src/main/java/com/huawei/ibooking/serivce/UserService.java
@@ -1,7 +1,10 @@
 package com.huawei.ibooking.serivce;
 
+import com.github.pagehelper.PageInfo;
 import com.huawei.ibooking.bean.po.User;
 import com.huawei.ibooking.bean.po.UserExample;
+import com.huawei.ibooking.bean.vo.user.SecurityUser;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +14,19 @@ public interface UserService {
 
     List<User> selectByExample(UserExample example);
 
+    PageInfo<SecurityUser> selectPageByExample(UserExample example, Integer pageNum, Integer pageSize, String sort);
+
     int insert(User record);
+
+    int deleteByExample(UserExample example);
+
+    int updateByExampleSelective(User record, UserExample example);
+
+    int updateByExample(User record, UserExample example);
+
+    int deleteByPrimaryKey(Long userId);
+
+
+    int updateByPrimaryKeySelective(User row);
 
 }
